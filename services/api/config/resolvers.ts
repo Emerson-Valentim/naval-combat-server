01bda355e195b53939712ae19e41cd9ea884c7ab
Emerson Valentim <emevalentim0@gmail.com>
2022-02-27 10:40:15 -0300

    feat: add socket emit on example resolver

--- services/api/config/resolvers.ts
+++ services/api/config/resolvers.ts
@@ -2,6 +2,8 @@
 import { exampleDomain } from "@naval-combat-server/domains";
 
 import { CLogger } from "../ports/logger";
+import SocketManager from "../ports/notification";
+import SocketHandler from "../ports/notification/handler";
 
 export const resolvers = {
   Query: {
@@ -10,6 +12,12 @@ export const resolvers = {
   Mutation: {
     example: async (_parent: any, _args: any) => {
       CLogger.info(`Reached API at ${new Date().getTime()}`);
+
+      const socket = new SocketHandler(SocketManager.get("naval-combat"));
+
+      socket.emit({
+        channel: "server:example", message: _args.input
+      });
 
       const result = await exampleDomain(_args.input.value);
 
