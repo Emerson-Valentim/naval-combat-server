7f0c77a1386b5188fe5459d88f47f9bf1705d74d
Emerson Valentim <emevalentim0@gmail.com>
2022-02-28 09:25:23 -0300

    fix: change jest config file to work with ts

--- packages/domains/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  verbose: true,
-  testRegex: "test.[jt]s$",
-  testPathIgnorePatterns: ["node_modules", "build"],
-};
-
-export default config;
