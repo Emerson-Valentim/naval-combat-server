9eb8c425dd017be17e249e24cebb7c6bd2e73113
Emerson Valentim <emevalentim0@gmail.com>
2022-02-28 09:00:00 -0300

    feat: add encrypt and decrypt methods

--- packages/domains/encryption/decrypt.ts
+++ packages/domains/encryption/decrypt.ts
@@ -1,7 +1,28 @@
+import jwt from "jsonwebtoken";
 import { curry } from "ramda";
 
-const decrypt = (message: string): string => {
-  return message;
+import SecretManagerDomain from "../secrets";
+
+const decrypt = async (SecretManager: typeof SecretManagerDomain, message: string): Promise<string> => {
+  if(typeof message !== "string") {
+    throw new Error("Message should be a string");
+  }
+
+  let secret: string;
+
+  try {
+    secret = await SecretManager.get("socket-private-key");
+  } catch (error: unknown) {
+    throw new Error(`Failed to get secret: ${(error as Error).message}`);
+  }
+
+  const encryptedMessage = jwt.verify(message, secret);
+
+  if(typeof encryptedMessage !== "string") {
+    return JSON.stringify(encryptedMessage);
+  }
+
+  return encryptedMessage;
 };
 
 export default curry(decrypt);
--- packages/domains/encryption/encrypt.ts
+++ packages/domains/encryption/encrypt.ts
@@ -1,7 +1,24 @@
+import jwt from "jsonwebtoken";
 import { curry } from "ramda";
 
-const encrypt = (message: string): string => {
-  return message;
+import SecretManagerDomain from "../secrets";
+
+const encrypt = async (SecretManager: typeof SecretManagerDomain, message: string): Promise<string> => {
+  if(typeof message !== "string") {
+    throw new Error("Message should be a string");
+  }
+
+  let secret: string;
+
+  try {
+    secret = await SecretManager.get("socket-private-key");
+  } catch (error: unknown) {
+    throw new Error(`Failed to get secret: ${(error as Error).message}`);
+  }
+
+  const encryptedMessage = jwt.sign(message, secret, { algorithm: "HS256" });
+
+  return encryptedMessage;
 };
 
 export default curry(encrypt);
