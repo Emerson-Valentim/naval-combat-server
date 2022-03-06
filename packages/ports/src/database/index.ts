5fd9e67cde7cbdd83d97101f9db0c00d1c8a6b70
Emerson Valentim <emevalentim0@gmail.com>
2022-03-06 13:01:39 -0300

    feat: add port package for database

---
+++ packages/ports/src/database/index.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+export default class Database {
+  private address: string;
+  private connection!: typeof mongoose;
+
+  constructor(address: string) {
+    this.address = address;
+  }
+
+  public async getEntity(_model: string, _schema: any) {
+    if (!this.isConnected()) {
+      await this.connect();
+    }
+
+    if (this.isModelRegistered(_model)) {
+      return mongoose.models[_model];
+    }
+
+    const schema = new Schema(_schema);
+
+    return mongoose.model(_model, schema);
+  }
+
+  private async connect(): Promise<void> {
+    if (this.isConnected()) return;
+
+    await mongoose.connect(this.address);
+
+    this.connection = mongoose;
+  }
+
+  private isConnected() {
+    return this.connection?.connection?.readyState === 1;
+  }
+
+  private isModelRegistered(_model: string) {
+    return !!mongoose.models[_model];
+  }
+}
