09f9ee037d8ca087404c5af47c07443c861da8fe
Emerson Valentim <emevalentim0@gmail.com>
2022-03-06 15:42:04 -0300

    refactor: add schema on create method

--- packages/domains/src/user/create.ts
+++ packages/domains/src/user/create.ts
@@ -1,8 +1,17 @@
+import Joi from "joi";
 import { curry } from "ramda";
 
-import UserDatabasePort, {UserInput} from "./ports/database";
+import UserDatabasePort, { UserInput } from "./ports/database";
+
+const createSchema = Joi.object({
+  email: Joi.string().email().required(),
+  username: Joi.string().min(8).required(),
+  password: Joi.string().min(8).required(),
+});
 
 const create = async (Database: typeof UserDatabasePort, input: UserInput) => {
+  createSchema.validate(input);
+
   const user = await Database.create(input);
 
   return user;
