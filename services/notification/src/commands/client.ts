ea363f074a0178444f267c122dbfdfb7c0ef6dea
Emerson Valentim <emevalentim0@gmail.com>
2022-03-19 16:58:20 -0300

    feat: add authenticator on client command

--- services/notification/src/commands/client.ts
+++ services/notification/src/commands/client.ts
@@ -1,18 +1,24 @@
+import { Socket } from "socket.io";
+
+import { authenticator } from "../config/tools";
 import { CLogger } from "../ports/logger";
 
 import { Command } from "./command";
 
 export default class Client implements Command {
-
-  public async execute(message: string) {
+  public async execute(message: string, socket: Socket) {
     try {
-      CLogger.info("Client command");
+      if (!socket.handshake.auth) {
+        return "";
+      }
+
+      await authenticator(socket.handshake.auth.token, socket.id);
 
       return JSON.parse(message);
     } catch (error: unknown) {
       CLogger.error({
-        message:(error as Error).message,
-        command: "client"
+        message: (error as Error).message,
+        command: "client",
       });
 
       return;
