07d71745f2af6bf2346b6f7f74251aa99fa6779d
Emerson Valentim <emevalentim0@gmail.com>
2022-05-08 13:57:04 -0300

    style: lint file

--- packages/domains/src/secrets/get.ts
+++ packages/domains/src/secrets/get.ts
@@ -1,10 +1,10 @@
 import { curry } from "ramda";
 
 const get = (key: string) => {
+  const secret =
+    process.env.APP_ENV === "local" ? `${key}_secret` : `${key}_sm_secret`;
 
-  const secret = process.env.APP_ENV === "local" ? `${key}_secret` : `${key}_sm_secret`;
-
-  if(!secret) {
+  if (!secret) {
     throw new Error("Secret is not available");
   }
 
