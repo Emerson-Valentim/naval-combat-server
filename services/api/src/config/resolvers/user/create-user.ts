ce7b48b4574d691fa877f56bfc90b84ae1d29611
Emerson Valentim <emevalentim0@gmail.com>
2022-05-15 21:17:06 -0300

    feat: inject socket on resolvers

--- services/api/src/config/resolvers/user/create-user.ts
+++ services/api/src/config/resolvers/user/create-user.ts
@@ -1,7 +1,13 @@
 import { user as UserDomain } from "@naval-combat-server/domains";
 
-const createUser = async (user: typeof UserDomain, input: any) => {
-  const newUser = await user.create(input);
+import { NavalCombatSocket as NavalCombatSocketPort } from "../../../ports/notification";
+
+const createUser = async (
+  user: typeof UserDomain,
+  NavalCombatSocket: typeof NavalCombatSocketPort,
+  input: any
+) => {
+  const newUser = await user.create(NavalCombatSocket, input);
 
   return newUser;
 };
