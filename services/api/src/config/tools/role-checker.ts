e4ec2ef4a0f358a8b7689750d4514629ef809b30
Emerson Valentim <emevalentim0@gmail.com>
2022-05-21 20:59:05 -0300

    fix: add optional chaining to avoid error

--- services/api/src/config/tools/role-checker.ts
+++ services/api/src/config/tools/role-checker.ts
@@ -1,9 +1,9 @@
 import { AuthToken } from "@naval-combat-server/domains/build/src/access-token/@types/auth-token";
 
 const roleChecker = (accessTokenData: AuthToken) => {
-  const isMaintainer = accessTokenData.roles.includes("maintainer");
+  const isMaintainer = accessTokenData?.roles?.includes("maintainer");
 
-  const isAdmin = accessTokenData.roles.includes("admin");
+  const isAdmin = accessTokenData?.roles?.includes("admin");
 
   return {
     isMaintainer, isAdmin
