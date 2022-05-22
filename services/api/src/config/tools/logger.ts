031e18fa97f96d998be5fdc5c24e8f91385a549a
Emerson Valentim <emevalentim0@gmail.com>
2022-05-22 11:08:52 -0300

    refactor: update logger class

--- services/api/src/config/tools/logger.ts
+++ services/api/src/config/tools/logger.ts
@@ -1,42 +1,43 @@
 import { ServerContext } from "../server";
 import { CLogger } from "../../ports/logger";
 
-const keysToMask = ["authorization", "password", "accessToken", "refreshToken", "base64"];
+const keysToMask = [
+  "authorization",
+  "password",
+  "accessToken",
+  "refreshToken",
+  "base64",
+];
 
 export default {
-  async requestDidStart({ request, context }: { request: any, context: ServerContext }) {
+  async requestDidStart({
+    request,
+    context,
+  }: {
+    request: any;
+    context: ServerContext;
+  }) {
     const startedAt = new Date().toISOString();
 
-    CLogger.log(
-      maskSensitiveData({ request, startedAt, context, level: "info" }, keysToMask)
+    CLogger.maskedLog(
+      { request, startedAt, context, level: "info" },
+      keysToMask
     );
     return {
-      async willSendResponse({ response, ...resto }: { response: any }) {
+      async willSendResponse({ response }: { response: any }) {
         const finishedAt = new Date().toISOString();
 
-        CLogger.log(
-          maskSensitiveData(
-            {
-              request,
-              response,
-              context,
-              finishedAt,
-              startedAt,
-            },
-            keysToMask
-          )
+        CLogger.maskedLog(
+          {
+            request,
+            response,
+            context,
+            finishedAt,
+            startedAt,
+          },
+          keysToMask
         );
       },
     };
   },
-};
-
-const maskSensitiveData = (data: any, fields: string[]) => {
-  const masked = fields.reduce((maskedString, field) => {
-    const regex = new RegExp(`(?<=${field}\\":\\")(.*?)(?=\\")`, "g");
-
-    return maskedString.replace(regex, "*");
-  }, JSON.stringify(data));
-
-  return JSON.parse(masked);
 };
--- services/api/src/ports/logger/index.ts
+++ services/api/src/ports/logger/index.ts
@@ -1,3 +1,21 @@
 import { CLogger as _CLogger } from "evs-tools";
 
-export const CLogger = _CLogger;
+class APILogger extends _CLogger {
+  public static maskedLog(message: unknown, keysToMask: string[]) {
+    const newMessage = APILogger.maskSensitiveData(message, keysToMask);
+
+    CLogger.log(JSON.stringify(newMessage, null, 2), true);
+  }
+
+  private static maskSensitiveData(data: any, fields: string[]) {
+    const masked = fields.reduce((maskedString, field) => {
+      const regex = new RegExp(`(?<=${field}\\":\\")(.*?)(?=\\")`, "g");
+
+      return maskedString.replace(regex, "*");
+    }, JSON.stringify(data));
+
+    return JSON.parse(masked);
+  }
+}
+
+export const CLogger = APILogger;
